50a208601a95418371982c154fed6a9e340fd248
Nathan Schomer <nathan@nathanschomer.com>
2021-04-15 20:41:28 -0700

    camera read & process works

--- camera_sensor.py
+++ camera_sensor.py
@@ -1,6 +1,9 @@
 from picarx_improved import PicarX
-from time import time
-from vilib import Vilib
+from time import time, sleep
+import cv2
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import numpy as np
 
 
 class Sensor:
@@ -8,12 +11,18 @@ class Sensor:
     def __init__(self, picar=PicarX(), sensitivity=0.5):
         self.sensitivity = sensitivity
         self.picar = picar
-        Vilib.camera_start(True)
-        Vilib.color_detect_switch(True)
-        Vilib.detect_color_name('blue')
+        self.camera = PiCamera()
+        sleep(0.1)
 
-    def read():
-        return
+    def read(self):
+        self.rawCapture = PiRGBArray(self.camera)
+        self.camera.capture(self.rawCapture, format="bgr")
+        img = self.rawCapture.array
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([30, 40, 0])
+        upper_blue = np.array([150, 255, 255])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        return mask
 
 
 class SensorProcessing:
@@ -21,24 +30,57 @@ class SensorProcessing:
     def __init__(self):
         pass
 
-    def process(self, data):
-        return
+    def sum_row(self, row):
+
+        net_dist = 0
+        thresh = 250
+
+        center_idx = row.shape[0] / 2
+
+        idxs = np.argwhere(row > thresh)[:, 0]
+
+        if len(idxs) == 0:
+            return 0
+        
+        for idx in [min(idxs), max(idxs)]:
+            net_dist += idx - center_idx
+
+        return net_dist/2
+
+
+    def process(self, img):
+
+        # canny edge detection
+        img = cv2.Canny(img, 200, 400)
+
+        # delete top half of image then flip
+        img = img[img.shape[0]//2:, :]
+        img = cv2.flip(img, 0)
+
+        # this essentially calculates distance from center for each row
+        dists = [self.sum_row(img[n, :]) for n in range(img.shape[0])]
+
+        mean_dist = sum(dists) / len(dists)
+        return img, mean_dist / (img.shape[1]//2)
 
 
 class Controller:
 
-    def __init__(self):
-        pass
+    def __init__(self, picar, scale=10):
+        self.picar = picar
+        self.scale = scale
 
-    def steer(self, cmd):
-        pass
+    def steer(self, direction):
+        angle = self.scale * direction
+        self.picar.set_dir_servo_angle(angle)
+        return angle
 
 
 if __name__ == "__main__":
 
     sensitivity = 0.99
     scale = 200
-    max_time = 10
+    max_time = 30
     speed = 20
 
     car = PicarX()
@@ -49,7 +91,8 @@ if __name__ == "__main__":
     # car.forward(speed)
     t = time()
     while time() - t < max_time:
-        vals = sensor.read()
-        # dir_val = proc.process(vals)
+        img = sensor.read()
+        mask, dir_val = proc.process(img)
+        print(dir_val)
         # angle = control.steer(dir_val)
     car.stop()
